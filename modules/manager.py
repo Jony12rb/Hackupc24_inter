1a39c9c0fee53b9189fff8c2aa194e2d6b4b6faf
JonathanRodBar <jonathan.rodr.barja@gmail.com>
2024-05-04 15:59:14 +0200

    Adapted manager

--- modules/manager.py
+++ modules/manager.py
@@ -2,12 +2,11 @@ from text_embedder import generate_text_embedding
 from db_info_retriever import retrieve_images
 from song_prompt_creator import create_song_prompt
 from song_generator import generate_song
-from video_generator import generate_videoclip
+from ImgsMus2Video import make_video
 
 def generate_videoclip(query: str):
     query_embedding = generate_text_embedding(query)
     images, image_descriptions = retrieve_images(query_embedding)
     song_prompt = create_song_prompt(query, image_descriptions)
     song = generate_song(song_prompt)
-    videoclip = generate_videoclip(images, song)
-    return videoclip
+    make_video(images, song)

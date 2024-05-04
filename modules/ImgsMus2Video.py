f454d59a8f96e289958fb47bbfb5ca7a1b43d120
JonathanRodBar <jonathan.rodr.barja@gmail.com>
2024-05-04 22:43:31 +0200

    New improved version of images + music to video

--- modules/ImgsMus2Video.py
+++ modules/ImgsMus2Video.py
@@ -2,7 +2,7 @@ import ffmpeg
 import os
 
 
-def make_video(image_names : list[str], audio_name : str, duration : int = 20 , video_path : str = 'Data/ExampleData/output.mp4', size = 5712 ):
+def make_video(image_names : list[str], audio_name : str, duration : int = 20 , video_path : str = 'Data/ExampleData/test.mp4', size = 5712 ):
     """
     Given a list of image names, an audio file, and a duration, creates a video with the images and audio.
     Each image will be displayed for the same amount of time (duration / number of images).
@@ -20,10 +20,35 @@ def make_video(image_names : list[str], audio_name : str, duration : int = 20 , 
 
     os.remove('in.txt')
     
+def make_video_experimental(image_names : list[str], audio_name : str, duration : int = 20 , video_path : str = 'Data/ExampleData/test.mp4', size = 5712 ):
+    """
+    Given a list of image names, an audio file, and a duration, creates a video with the images and audio.
+    Each image will be displayed for the same amount of time (duration / number of images).
+    """
+    n = len(image_names)
+    time_per_image = duration / n
+    fade_duration = 0.1*time_per_image
+    fade_out = time_per_image - fade_duration
+    command = "ffmpeg"
+    for image in image_names:
+        command += f" -loop 1 -t {time_per_image} -i {image}  "
+    command += f" -i {audio_name} -filter_complex '"
+    for i in range(n):
+        if i == 0:
+            command += f"[{i}:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1,fade=t=out:st={fade_out}:d={fade_duration}[v{i}]; "
+        else:
+            command += f"[{i}:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1,fade=t=in:st=0:d={fade_duration},fade=t=out:st={fade_out}:d={fade_duration}[v{i}]; "
+    for i in range(n):
+        command += f"[v{i}]"
+        
+    command += f"concat=n={n}:v=1:a=0,format=yuv420p[v]' -map '[v]' -map {n}:a -shortest {video_path}"
+    
+    os.system(command)
+    
     
 if __name__ == '__main__':
     images = ["Data/PngRealSet/IMG_4383.png", "Data/PngRealSet/IMG_3723.png", "Data/PngRealSet/IMG_3767.png"]
     audio = "Data/ExampleData/audio.mp3"
-    t = 5
-    make_video(images, audio, t)
+    t = 10
+    print(make_video_experimental(images, audio, t))
     
